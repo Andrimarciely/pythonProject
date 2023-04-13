dd68fdcb224f33270a750b0899dd7527f18e49a5
Andrimarciely Souza <andrimarciely@gmail.com>
2023-04-13 14:17:49 -0400

    modelo de parametros

--- dados/models.py
+++ dados/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Parametro(models.Model):
+
+    descricao_parametro = models.CharField(max_length=100, help_text='nome do agente químico')
+    unidade_de_medida_do_parametro = models.CharField(max_length=10, help_text='unidade de medida do agente químico')
+    resultado = models.CharField(max_length=10, help_text='valor mensurado do agente químico na amostra')
+    lq = models.CharField(max_length=10, help_text='valor de referência')
+
+    def __str__(self):
+        return self.descricao_parametro
--- simplimsWeb/settings.py
+++ simplimsWeb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dados',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
