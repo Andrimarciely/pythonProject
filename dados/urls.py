d8afaedf35def5cd9e7d327b59a40445b128847d
Andrimarciely Souza <andrimarciely@gmail.com>
2023-04-16 14:23:12 -0400

    fix - listagem de solicitacao de servico

--- dados/urls.py
+++ dados/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='solicitacoes-de-servicos-all'),
- #   path('<int:id>', views.show, name='asolicitacao-de-servico-show'),
- #   path('<int:year>/<int:month>/<int:day>', views.day, name='solicitacao-de-servico-day'),
+    path('<int:id>', views.show, name='solicitacao-de-servico-show'),
+    path('<int:year>/<int:month>/<int:day>', views.day, name='solicitacao-de-servico-day'),
     ]
--- dados/views.py
+++ dados/views.py
@@ -2,16 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import SolicitacaoDeServico
 
+
 def index(request):
     """Exibe todas as solicitações de serviços consolidados em uma única página. Não recebe parâmetros"""
     context = {
         'solicitacoes_de_servicos': SolicitacaoDeServico.objects.order_by('data_de_entrega_do_relatorio', 'prioridade'),
     }
-    return render(request,'solicitacoes_de_servicos.html', context)
+    return render(request, 'solicitacoes_de_servicos.html', context)
+
 
 def show(request, id):
     """Visualização de um determinado evento, recebe o 'id' do evento."""
     context = {
-            'event': SolicitacaoDeServico.objects.get(id=id),
-            }
+        'solicitacoes_de_servicos': SolicitacaoDeServico.objects.get(id=id),
+    }
     return render(request, 'show.html', context)
+
+
+def day(request, year, month, day):
+    """Visualização dos eventos de um determinado dia, recebe a data em
+    formato ano/mês/dia como parâmtro."""
+    #
+    day = datetime(year, month, day)
+    context = {
+        'today': localdate(),
+        'day': day,
+        'previous': day - timedelta(days=1),
+        'next': day + timedelta(days=1),
+        'solicitacoes_de_servicos': SolicitacaoDeServico.objects.filter(
+            date="{:%Y-%m-%d}".format(day)).order_by('-priority', 'solicitacoes_de_servicos')
+    }
+    return render(request, 'day.html', context)
